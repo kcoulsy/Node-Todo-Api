7d5eb2a8977ad0782e8b02adbfd1d09a5954dbaa
Kristian <kristian@coulsy.co.uk>
2018-03-08 18:02:59 +0000

    Added PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const _ = require('lodash');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -70,6 +71,31 @@ app.delete('/todos/:id', (req, res) => {
       //if doc then send back with 200
     //error
       //400 with empty body
+});
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text','complete']);
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  if(_.isBoolean(body.complete) && body.complete){
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completedAt = null;
+    body.complete = false
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 app.listen(port, () => {
